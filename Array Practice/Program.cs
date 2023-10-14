284f17c5d2a2b8938167f0657e5359f57e8050d4
Nurlan <nurlanea@code.edu.az>
2023-10-14 22:56:38 +0400

    upload

---
+++ Array Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿#region Sum
+//int[] numbers = { 1, 2, 3 };
+//int sum = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+
+//Console.WriteLine(sum);
+
+#endregion
+
+#region 10-dan yuxari
+//int[] n = { 7, 8, 9, 10, 11, 12, 13 };
+//int count = 0;
+
+//for (int i = 0; i < n.Length; i++)
+//{
+//    if (n[i] > 10)
+//    { count++; }
+//}
+//Console.WriteLine(count);
+
+//int[] numbers = { 10, 20, 30 };
+//int sum = 0;
+#endregion 
+
+#region sum/n
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+//}
+
+//Console.WriteLine(sum / numbers.Length);
+#endregion
+
+#region Aylar
+string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+foreach (var item in months)
+{
+    if (item == "Mar" || item == "Apr" || item == "May")
+    {
+        Console.WriteLine("Yaz Aylari");
+    }
+}
+
+#endregion
